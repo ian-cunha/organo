0e2111f7cf5b6fb97ac6aa0a551c3db85ebe5e6f
Ian <contatoiancunha@gmail.com>
2022-12-13 16:38:51 -0300

    New Update 13/12

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -3,8 +3,11 @@ import './Banner.css'
 export const Banner = () => {
   // JSX
   return (
-    <header className='banner'>
-    <img src="/imagens/banner.png" alt="Banner principal da página do Organo"/>
-    </header>
+    <>
+      <header className='banner'>
+        <img src="/imagens/banner.png" alt="Banner principal da página do Organo" />
+      </header>
+      <h1>Teste</h1>
+    </>
   )
 }
